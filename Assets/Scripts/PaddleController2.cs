b40e210b9c9446a033831361df06cd396c072a0c
edim dendy <edimdendy@gmail.com>
2020-01-02 13:58:13 -0800

    fix game

---
+++ Assets/Scripts/PaddleController2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController2 : MonoBehaviour
+{
+    public int kecepatan = 7;
+    public string axis = "Vertical2";
+    public double batasAtas = 2.8;
+    public double batasBawah = -2.8;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float gerak = Input.GetAxis(axis) * kecepatan * Time.deltaTime;
+        float nextPos = transform.position.y + gerak;
+        if (nextPos > batasAtas) {
+            gerak = 0;
+        }
+        if (nextPos < batasBawah) {
+            gerak = 0;
+        }
+        transform.Translate(0, gerak, 0);
+    }
+}
